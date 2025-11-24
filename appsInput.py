089456785c7df4aa9a841f55c1152ea2bd99d5b7
Blake Hannaford <blake.hannaford@gmail.com>
2025-11-23 16:12:30 -0800

    new menu system, merging, and sorting

--- appsInput.py
+++ appsInput.py
@@ -8,11 +8,13 @@ def error(msg):
 
 ifURL = 'https://facultysearch.interfolio.com/28343/positions/171499/'
 TESTROW = -1  # <0 = no test printout
+SORTING = False
 
+# define col headers of our working files
+outputCols = ['Firstname', 'Lastname', 'Teaching', 'Research', 'WIRC', 'Id','Link', 'Last date', 'Which area']
 
 def help():
     print(f'''
-
     Log into {ifURL}
     Click "Reports" on left menu.
     On "Position Names": Select 171499.
@@ -24,19 +26,33 @@ def help():
                 Which Area best describes
                 (Main Research Area
                 Cross-cutting Area(s))
+    Download CSV
+
+    then:
+
+    >appsInput.py   [Downloaded csv filename]
 
         ''')
 
 
 class applicant:
-    def __init__(self,fn,ln,iD,aD,ar):
+    def __init__(self,fn,ln,iD,aD,ar,scores=None):
         self.fName = fn
         self.lName = ln
+        if scores is not None:
+            self.teaching = scores[0]
+            self.research = scores[1]
+            self.wirc     = scores[2]
+        else:
+            self.teaching = None
+            self.research = None
+            self.wirc     = None
+
         self.iD = iD
         # self.url = 'https://facultysearch.interfolio.com/28343/positions/171499/applications/' + iD
         self.url = ifURL + iD
         self.appDate = aD
-        self.createDate = dt.datetime.today()
+        # self.createDate = dt.datetime.today()
         self.area = ar
 
     def __repr__(self):
@@ -46,26 +62,72 @@ class applicant:
         t += f'{self.iD:9} '
         t += f'{self.appDate:15} '
         t += f'{self.area:20}'
-        t += f'{self.createDate.strftime('%Y-%m-%d'):15}'
+        # t += f'{self.createDate.strftime('%Y-%m-%d'):15}'
         return t
 
     def genSSRow(self):
     #
     # # desired output cols, in order
     # outputCols = ['Firstname', 'Lastname', 'Teaching', 'Research', 'WIRK', 'Id','Link', 'Last date', 'Which area']
-        r = [self.fName, self.lName,  '', '', '',self.iD, self.url, self.appDate,self.area]
+        s1 = self.teaching
+        s2 = self.research
+        s3 = self.wirc
+        # reviewer scores if present
+        if (s1 ) is None:
+            s1 = ''
+        if (s2 ) is None:
+            s2 = ''
+        if (s3 ) is None:
+            s3 = ''
+        r = [self.fName, self.lName,  s1, s2, s3, self.iD, self.url, self.appDate,self.area]
         return r
 
-def readDownload(ifp):
 
-    data = csv.reader(ifp)
+
+def readWorkingFile(inFileName):
+
+    #
+    #  read in a working file
+    #
+    f = open(inFileName, 'r',encoding='utf-8-sig')
+
+    data = csv.reader(f)
+
+    # read the input header
+    inputHeaderRow = next(data)
+    for i,c in enumerate(inputHeaderRow):
+        if (c != outputCols[i]):
+            print('input Working file header: ', inputHeaderRow)
+            print('               outputCols: ', outputCols)
+            error('readWorking: input header mismatch')
+    existApps = []
+    for r in data:
+        fn  = r[0]
+        ln  = r[1]
+        tch = r[2]
+        rsr = r[3]
+        wir = r[4]
+        idn = r[5]
+        lk  = r[6]
+        ad  = r[7]
+        ar  = r[8]
+        tmp = applicant(fn,ln,idn,ad,ar, scores=[tch,rsr,wir])
+        existApps.append(tmp)
+    return existApps
+
+def readDownload(inFileName):
+    #
+    #  read in a downloaded file
+    #
+    f = open(inFileName, 'r',encoding='utf-8-sig')
+
+    data = csv.reader(f)
 
     # expected input columns (possibly among others)
     inputHeads = ['Firstname', 'Lastname', 'Last date', 'Which','Id','Position']
 
     # desired output  order of the input cols we select
     selectedCols = ['Firstname', 'Lastname', 'Id', 'Last date', 'Which area']
-    outputCols = ['Firstname', 'Lastname', 'Teaching', 'Research', 'WIRK', 'Id','Link', 'Last date', 'Which area']
 
     # read the input header
     inputHeaderRow = next(data)
@@ -105,36 +167,40 @@ def readDownload(ifp):
 
 
     print(f'parsed and loaded {len(applicants)} applicants')
-    ifp.close()
 
-    return applicants, outputCols
-
-def main():
-    if not (len(sys.argv) ==2):
-        error('need command line arg.')
-
-    if sys.argv[1] == 'help':
-        help()
-        quit()
-
-    fname = sys.argv[1]
-    f = open(fname, 'r',encoding='utf-8-sig')
-
-    applicants, outputCols= readDownload(f)
+    f.close()
 
     if TESTROW > 0:
         print(applicants[TESTROW])
         print('')
         print(applicants[TESTROW].genSSRow())
 
+    return applicants
+
+
+def merge(apps1, apps2):
+    """Merge two lists of applicant objects, removing duplicates by iD. (thanks Claude)"""
+    seen = {app.iD for app in apps1}
+    return apps1 + [app for app in apps2 if app.iD not in seen]
+
+def sortApps(applicants):
+    print('Sorting (got here)')
+    """Sort applicants by date, then by iD."""
+    return sorted(applicants, key=lambda app: (dt.datetime.strptime(app.appDate.strip(), '%m/%d/%Y'), app.iD))
+
+def writeOut(applicants, ofn, outHeader):
     #
-    #  Generate new output file (sorted date, then by ID)
+    #  Save new output file (sorted date, then by ID)
     #
-    ofn = 'newsheet.csv'
+    # ofn = 'newsheet.csv'
+    if SORTING:
+        applicants = sortApps(applicants).copy()
+
     of = open(ofn, 'w')
     writer = csv.writer(of)
 
-    writer.writerow(outputCols )
+    #write out the header row
+    writer.writerow(outHeader )
     for a in applicants:
         row = a.genSSRow()
         writer.writerow(row)
@@ -145,14 +211,90 @@ def main():
 def menu(mdat):
     try:
         x = mdat['labels']
-        x = mdat['meth']
+        # x = mdat['meth']
         # x = mdat['style']
+        nitems = len(mdat['labels'])
     except:
         error('Bad menu inputs:', mdat)
     i = 1
+    # print menu header
+    #
+    statuskeys = mdat['state'].keys()
+    for k in statuskeys:
+        print(f"{k:10} : {mdat['state'][k]} ",end='')
+        print('')
+    #
+    # print the choices
+    #
     for mi in mdat['labels']:
         print(f'{i:3}: {mi:20}')
         i+=1
+    resp = input('Select: ')
+    if resp == '':
+        quit()
+    try:
+        j = int(resp)-1
+        assert int(j) < nitems
+    except:
+        error(f'unusable response: {resp}')
+    return j, mdat['labels'][j]
+
+#
+#
+#
 if __name__ == '__main__':
-    main()
+
+    if not (len(sys.argv) ==2):
+        error('need command line arg.')
+
+    if sys.argv[1] == 'help':
+        help()
+        quit()
+
+
+    #
+    #  set up and run the menu
+    #
+
+    mitems = ['Read, and Convert Download .csv',
+              'Read, convert, and MERGE download .csv',
+              'Set sorting on output save',
+              'Clear sorting on output save',
+              'Quit']
+
+    mdata = {'labels':mitems, 'state':{'sorting':SORTING}}
+    while True:
+        j, selcmd = menu(mdata)
+
+        # read in a download csv and produce a shareable sheet
+        if j==0:
+            # readConvert(sys.argv[1])
+            ofn = 'newsheet.csv'
+            applicants = readDownload(sys.argv[1])
+            writeOut(applicants, ofn, outputCols)
+
+        # read in a download csv and merge it with an existing shareable sheet
+        if j==1:
+            newfn = sys.argv[1]
+            newapps = readDownload(newfn)
+            oldfn = input('Enter existing working csv: ')
+            oldapps= readWorkingFile(oldfn.strip())
+            updatedApps = merge(newapps, oldapps)
+            ofn = 'newsheetMerge.csv'
+            writeOut(updatedApps, ofn, outputCols)
+
+        if selcmd.startswith('Set'):
+            SORTING = True
+            print('\nSorting is ENABLED')
+            mdata['state']['sorting']=True
+        if selcmd.startswith('Clear'):
+            SORTING = False
+            print('\nSorting is DISABLED')
+            mdata['stat']['sorting']=False
+
+        if selcmd.upper().startswith('Q'):
+            quit()
+
+        x = input('Continue...<enter>')
+
 
